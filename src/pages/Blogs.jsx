75d5478478823495f246016eec729004703af7f2
umutplvn <umutpehlivan2078@gmail.com>
2024-07-23 05:45:05 +0100

    up

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -23,6 +23,7 @@ import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import useAuthCall from "../hooks/useAuthCall";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Blogs = () => {
   const { getBlogs, likeBlog } = useDataCall();
@@ -97,7 +98,6 @@ const Blogs = () => {
         {filteredBlogs?.map((item) => {
           return (
             <Card
-            onClick={()=>navigate(`/blogs/${item._id}`)}
               variant="outlined"
               sx={{
                 maxWidth: "310px",
@@ -111,7 +111,36 @@ const Blogs = () => {
                 },
               }}
             >
+              
               <CardContent>
+              <Box
+                  sx={{
+                    position: "absolute",
+                    right:"1rem",
+                    "&::before": {
+                      content: '""',
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      bottom: 0,
+                      right: 0,
+                      m: "-2px",
+                      borderRadius: "50%",
+                      background:
+                        "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+                    },
+                  }}
+                >
+                  
+                  <Avatar
+                    size="sm"
+                    src={defiIcon}
+                    sx={{
+                      border: "2px solid",
+                      borderColor: "background.body",
+                    }}
+                  />
+                </Box>
                 <Typography
                   sx={{
                     fontSize: "1rem",
@@ -121,10 +150,12 @@ const Blogs = () => {
                     WebkitBoxOrient: "vertical",
                     WebkitLineClamp: 2,
                     textOverflow: "ellipsis",
+                    mr:"2rem"
                   }}
                 >
                   {item?.title}
                 </Typography>
+            
               </CardContent>
 
               <CardOverflow>
@@ -245,7 +276,16 @@ const Blogs = () => {
                   alignItems: "center",
                 }}
               >
-                <Link
+            <Typography
+            type="submit"
+            variant="contained"
+            onClick={()=>navigate(`/blogs/${item._id}`)}
+            style={{color:"#ff5100", fontSize:"1rem"}}
+          >
+            Read More
+          </Typography>
+
+              <Link
                   component="button"
                   underline="none"
                   fontSize="10px"
@@ -253,32 +293,7 @@ const Blogs = () => {
                 >
                   {formatDateTime(item.createdAt)}
                 </Link>
-                <Box
-                  sx={{
-                    position: "relative",
-                    "&::before": {
-                      content: '""',
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      bottom: 0,
-                      right: 0,
-                      m: "-2px",
-                      borderRadius: "50%",
-                      background:
-                        "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
-                    },
-                  }}
-                >
-                  <Avatar
-                    size="sm"
-                    src={defiIcon}
-                    sx={{
-                      border: "2px solid",
-                      borderColor: "background.body",
-                    }}
-                  />
-                </Box>
+              
               </CardContent>
             </Card>
           );
