3cc7b6b727e421095100c7206dff0d3d39f028fa
umutplvn <umutpehlivan2078@gmail.com>
2024-07-28 07:08:29 +0100

    up

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,10 +3,23 @@ import axios from "axios";
 import io from "socket.io-client";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import { Box, Typography, TextField, Button, List, ListItem, Avatar } from "@mui/material";
+import LocalSeeIcon from '@mui/icons-material/LocalSee';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  Avatar,
+  IconButton,
+} from "@mui/material";
 import { MessageBox } from "react-chat-elements";
 import "react-chat-elements/dist/main.css";
-import ReplyRoundedIcon from '@mui/icons-material/ReplyRounded';
+import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
+import { toast } from "react-hot-toast";
 const socket = io("https://defi-chat-backend.onrender.com");
 
 const Chat = () => {
@@ -17,11 +30,16 @@ const Chat = () => {
   const [image, setImage] = useState(null);
   const [video, setVideo] = useState(null);
   const user = users?.filter((user) => user._id === chatUserId);
-const navigate=useNavigate()
+  const navigate = useNavigate();
+  const [fileName, setFileName] = useState('');
+const [loading, setLoading] = useState("Loading")
+
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await axios.get(`https://defi-chat-backend.onrender.com/api/chats/${userId}/${chatUserId}`);
+        const response = await axios.get(
+          `https://defi-chat-backend.onrender.com/api/chats/${userId}/${chatUserId}`
+        );
         setMessages(response.data);
       } catch (error) {
         console.error("Failed to fetch messages:", error);
@@ -33,7 +51,8 @@ const navigate=useNavigate()
 
     socket.on("message", (newMessage) => {
       if (
-        (newMessage.receiverId === userId && newMessage.senderId === chatUserId) ||
+        (newMessage.receiverId === userId &&
+          newMessage.senderId === chatUserId) ||
         (newMessage.receiverId === chatUserId && newMessage.senderId === userId)
       ) {
         setMessages((prevMessages) => [newMessage, ...prevMessages]);
@@ -45,7 +64,10 @@ const navigate=useNavigate()
     };
   }, [userId, chatUserId]);
 
+
+
   const handleSubmit = async (e) => {
+    setMessages([...messages, loading]);
     e.preventDefault();
     const formData = new FormData();
     formData.append("senderId", userId);
@@ -59,20 +81,34 @@ const navigate=useNavigate()
     }
 
     try {
-      const response = await axios.post("https://defi-chat-backend.onrender.com/api/messages", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const response = await axios.post(
+        "https://defi-chat-backend.onrender.com/api/messages",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       setMessages([...messages, response.data]);
       setMessage("");
       setImage(null);
       setVideo(null);
     } catch (error) {
       console.error("Failed to send message", error);
+      toast("Failed to send message.")
     }
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFileName(file ? file.name : '');
+    if (file && file.type.startsWith("image/")) {
+      setImage(file);
+    } else if (file && file.type.startsWith("video/")) {
+      setVideo(file);
+    }
+  };
 
   return (
     <>
@@ -83,15 +119,27 @@ const navigate=useNavigate()
           gap: "0.8rem",
           backgroundColor: "#fefefe",
           padding: "1rem",
-          paddingLeft:{ xs: "1rem", sm: "6rem", md: "11.5rem" },
+          paddingLeft: { xs: "1rem", sm: "6rem", md: "11.5rem" },
           boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
-          borderBottomLeftRadius:"0.7rem",
-          borderBottomRightRadius:"0.7rem",
+          borderBottomLeftRadius: "0.7rem",
+          borderBottomRightRadius: "0.7rem",
+          height:"4rem"
         }}
       >
-        <Box sx={{color:"#FE5E00", height:"100%", width:"1rem", display:"flex", alignItems:"start", cursor:"pointer", mb:"1rem"}} onClick={()=>navigate('/chats')}>
-
-<ReplyRoundedIcon/>
+        <Box
+          sx={{
+            color: "#FE5E00",
+            height: "100%",
+            width: "1rem",
+            display: "flex",
+            alignItems: "start",
+            cursor: "pointer",
+            mb:"0.8rem",
+            mr:"0.5rem"
+          }}
+          onClick={() => navigate("/chats")}
+        >
+          <ReplyRoundedIcon sx={{fontSize:"2rem"}} />
         </Box>
         <Avatar src={user[0].avatar} />
         <Typography>{user[0].name}</Typography>
@@ -100,98 +148,140 @@ const navigate=useNavigate()
       <Box
         sx={{
           paddingLeft: { xs: "0", sm: "4rem", md: "9rem" },
+          mb:"8rem"
         }}
       >
         <List>
-          {messages
-            ?.slice()
-            .map((msg, index) => (
-              <ListItem
-                key={index}
-                alignItems="flex-start"
-                sx={{
-                  display: "flex",
-                  justifyContent: msg.senderId === userId ? "flex-end" : "flex-start",
-                }}
-              >
-                {msg.message && (
-                  <MessageBox
-                    position={msg.senderId === userId ? "right" : "left"}
-                    type="text"
-                    text={msg.message}
-                    date={new Date(msg.timestamp)}
+          {messages?.slice().map((msg, index) => (
+            <ListItem
+              key={index}
+              alignItems="flex-start"
+              sx={{
+                display: "flex",
+                justifyContent:
+                  msg.senderId === userId ? "flex-end" : "flex-start",
+              }}
+            >
+              {msg.message && (
+                <MessageBox
+                  position={msg.senderId === userId ? "right" : "left"}
+                  type="text"
+                  text={msg.message}
+                  date={new Date(msg.timestamp)}
+                />
+              )}
+              {msg.image && (
+                <Box
+                  sx={{
+                    maxWidth: "200px",
+                    maxHeight: "320px",
+                    borderRadius: "0.5rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    overflow: "hidden",
+                    boxShadow:
+                      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+                  }}
+                >
+                  <img
+                    src={`https://defi-chat-backend.onrender.com/uploads/${msg.image}`}
+                    alt="Uploaded"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
                   />
-                )}
-                {msg.image && (
-                  <Box
-                    sx={{
-                      maxWidth:"200px",
-                      maxHeight:"320px",
-                    borderRadius:"0.5rem",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      overflow: "hidden",
-                      boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+                </Box>
+              )}
+              {msg.video && (
+                <Box
+                  sx={{
+                    maxWidth: "250px",
+                    maxHeight: "320px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "0.5rem",
+                    overflow: "hidden",
+                    paddingLeft: { xs: "1rem", sm: "6rem", md: "11.5rem" },
+
+                    boxShadow:
+                      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+                  }}
+                >
+                  <video
+                    controls
+                    src={`https://defi-chat-backend.onrender.com/uploads/${msg.video}`}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
                     }}
-                  >
-                   
-                    <img
-                      src={`https://defi-chat-backend.onrender.com/uploads/${msg.image}`}
-                      alt="Uploaded"
-                      style={{ width: "100%", height: "100%", objectFit: "contain" }}
-                    />
-                  </Box>
-                )}
-                {msg.video && (
-                  <Box
-                    sx={{
-                    
-                      maxWidth:"250px",
-                      maxHeight:"320px",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      borderRadius:"0.5rem",
-                      overflow: "hidden",
-                      boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
-                    }}
-                  >
-                    <video
-                      controls
-                      src={`https://defi-chat-backend.onrender.com/uploads/${msg.video}`}
-                      style={{ width: "100%", height: "100%", objectFit: "contain" }}
-                    />
-                  </Box>
-                )}
-              </ListItem>
-            ))}
+                  />
+                </Box>
+              )}
+            </ListItem>
+          ))}
         </List>
-        <form
+        <Box>
+
+        <Box
+          component="form"
           onSubmit={handleSubmit}
-          style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
-        >
-          <TextField
-            variant="outlined"
-            fullWidth
-            placeholder="Type a message"
-            value={message}
+          sx={{ display: "flex", gap: "0.5rem", padding:"1rem", 
+          width:"100%",
+          alignItems:"center",
+          height:"3.7rem",
+          zIndex:"4",
+          backgroundColor:"white",
+          borderTopLeftRadius:"1rem",
+          borderTopRightRadius:"1rem",
+          boxShadow:
+          "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px", position:"fixed", bottom:"0rem", right:"0" }}
+        > 
+        <input
+        type="file"
+        id="file-upload"
+        accept="image/*,video/*"
+        onChange={handleFileChange}
+        style={{ display: 'none' }}
+      />
+      <label htmlFor="file-upload">
+        <IconButton  component="span">
+          <LocalSeeIcon sx={{color:"black"}} />
+        </IconButton>
+      </label>
+     
+      <TextField
+        variant="outlined"
+        fullWidth
+        sx={{
+          '& .MuiInputBase-root': {
+            height: '2rem',
+            padding: 0,
+            borderRadius:"2rem"
+          },
+          '& .MuiOutlinedInput-input': {
+            height: '2rem',
+            padding: '0 14px',
+          },
+          '& .MuiInputLabel-root': {
+            height: '2rem',
+            lineHeight: '2rem',
+          },
+        }}
+        placeholder="Type..."
+        value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-          <input
-            type="file"
-            accept="video/*"
-            onChange={(e) => setVideo(e.target.files[0])}
-          />
-          <Button type="submit" variant="contained">
-            Send
-          </Button>
-        </form>
+         
+         <Button type="submit" sx={{backgroundColor:"#fefefe", color:"black"}}>
+         <SendRoundedIcon />
+      </Button>
+        </Box>
+        </Box>
       </Box>
     </>
   );
