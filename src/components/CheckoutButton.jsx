8caf96beb547bf9eb4797fbef267759ea8450bf8
umutplvn <umutpehlivan2078@gmail.com>
2024-08-15 00:31:03 +0100

    up

--- src/components/CheckoutButton.jsx
+++ src/components/CheckoutButton.jsx
@@ -5,14 +5,14 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 const CheckoutButton = () => {
-  const {userId} =useSelector((state)=>state.auth) 
+  const {userId, email} =useSelector((state)=>state.auth) 
   const priceId = "price_1PnABkP3dSOC4uCmWjEsUf3p";
   const publicKey = "pk_test_51Pn9FyP3dSOC4uCmkRJQFsEvnDJQSLarGeZzKIjjYh488gsvLNNUyHjToy1o6BlQ3aHw8GfAnnVzHYx3AP1dBHiv00GG187Rwo";
 
   const handleClick = async () => {
     try {
-      const response = await axios.post(`https://defi-fitness-api.onrender.com/api/create-checkout-session`, { priceId, userId });
-
+      const response = await axios.post(`https://defi-fitness-api.onrender.com/api/create-checkout-session`, { priceId, userId, email });
+console.log("response", response);
       const { sessionId } = response.data;
 
       const stripe = window.Stripe(publicKey);
